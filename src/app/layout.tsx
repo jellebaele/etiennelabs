87c20685b1b3b0b6f49ce753dc83a30162dac601
jellebaele <jellebaele@hotmail.com>
2026-03-31 19:22:37 +0200

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import Header from '@/components/Header';
 import type { Metadata } from 'next';
 import './globals.css';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'etiennelab.dev',
@@ -15,7 +19,7 @@ export default function RootLayout({
   return (
     <html
       lang='en'
-      suppressHydrationWarning>
+      suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body>
         <div className='min-h-screen bg-background max-w-7xl mx-auto px-8'>
           <div className='grain-overlay' />
